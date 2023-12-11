eacab5d96e00a977b65ad72470eb5b36e6260d40
fo-yyhhh <143651498+fo-yyhhh@users.noreply.github.com>
2023-12-11 16:36:19 -0500

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -1,13 +1,15 @@
 import * as THREE from 'https://unpkg.com/three@latest/build/three.module.js';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 150, window.innerWidth / window.innerHeight, 0.01, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-camera.position.z = 5;
+camera.position.x = 0;
+camera.position.y = 10;
+camera.position.z = 25;
 
 function animate() {
 
