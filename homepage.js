ae390a43d7245d54b10c37ad69d9acc27be9ac8c
fo-yyhhh <143651498+fo-yyhhh@users.noreply.github.com>
2023-12-11 21:24:15 -0500

    earth

--- homepage.js
+++ homepage.js
@@ -1,31 +1,51 @@
 import * as THREE from 'https://unpkg.com/three@latest/build/three.module.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'https://unpkg.com/three@latest/examples/jsm/controls/OrbitControls.js';
+
+
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 150, window.innerWidth / window.innerHeight, 0.01, 1000 );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
+
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 camera.position.x = 0;
-camera.position.y = 10;
-camera.position.z = 25;
-
-function animate() {
-
-	requestAnimationFrame( animate );
+camera.position.y = 0;
+camera.position.z = 18;
 
 
-	renderer.render( scene, camera );
+const SceneLight = new THREE.AmbientLight(0xffffff, 1); // light & strength
+scene.add(SceneLight);
 
-}
+//add directionlight to show the shapes
+const directionalLight1 = new THREE.DirectionalLight(0xffffff, 3);
+directionalLight1.position.set(1, 1, 1); 
+scene.add(directionalLight1);
 
-animate()
+const directionalLight2 = new THREE.DirectionalLight(0xffffff, 4);
+directionalLight2.position.set(-1, -1, -1); 
+scene.add(directionalLight2);
+
+const pointLight = new THREE.PointLight(0xffffff, 3, 20); //strength & distance
+pointLight.position.set(0, 0, 5);
+scene.add(pointLight);
 
 
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableZoom = false;
+controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+controls.dampingFactor = 0.25;
+controls.screenSpacePanning = false;
+
+
+
+
 
 const loader = new GLTFLoader();
+/*
 loader.load('../mbti figures/scene.gltf', function (gltf){
         scene.add(gltf.scene);
     },
@@ -36,3 +56,31 @@ loader.load('../mbti figures/scene.gltf', function (gltf){
         console.error(error);
     }
 );
+*/
+
+loader.load('../earth/scene.gltf', function (gltf){
+    scene.add(gltf.scene);
+},
+(xhr) => {
+    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+},
+(error) => {
+    console.error(error);
+}
+);
+
+
+
+
+function animate() {
+
+	requestAnimationFrame( animate );
+
+    controls.update();
+
+	renderer.render( scene, camera );
+
+}
+
+animate()
+
